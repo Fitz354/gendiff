40768006b4c74500b82c8d1351f3e3750f9f879b
Fitz354 <lozovsky.alx@yandex.ru>
2017-11-06 13:02:35 +0300

    remove unnesessarty string

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ commander
   .version('0.1.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format');
 
 export default () => commander.parse(process.argv);
